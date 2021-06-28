35ac629e0d5f81ab5bd6196aa7cc2cf32f365183
Hasan Al Bantani <86176675+hasanalbantani@users.noreply.github.com>
2021-06-28 21:51:44 +0700

    Add files via upload

---
+++ latihan4.php
@@ -0,0 +1,13 @@
+<?php
+	//ini untuk  mencetak tulisan
+	echo "hello world! <br>";
+	
+	//ini untuk mencetak angka
+	echo "aku adalah PHP!<br>";
+
+
+	$nilai1 = 42342343;
+	$nilai2 = 324234;
+	$jumlah = $nilai1 * $nilai2;
+	echo 'hasil dari 42342343 x 324234 adalah = '.$jumlah;
+?>
